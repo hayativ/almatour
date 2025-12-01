02ba3d9258ae955fc3918019c9f6ef42813872a9
Yeva Davydova <davydova.04.eva@mail.ru>
2025-12-01 22:39:38 +0500

    Modify unit tests

--- apps/users/tests/test_simple.py
+++ apps/users/tests/test_simple.py
@@ -1,63 +1,43 @@
+from apps.users.tools import Tool, Calculator
 import pytest
-from apps.users.tools import User, UserRepository
-from typing import Optional
-from conftest import Settings
-
-@pytest.fixture
-def my_fixture() -> str:
-    return "Hello, Fixture!"
+from contextlib import nullcontext as does_not_raise
+from typing import Any
 
 
-def test_using_fixture(my_fixture: str) -> None:
-    my_user_ids = [1, 2, 3]
-    assert len(my_user_ids) == 3, "Length of my_user_ids must be 3"
-    assert my_fixture == "Hello, Fixture!", f"{my_fixture} must be equal 'Hello, Fixture!'"
+def test_example():
+    assert 1 == 1, "1 must be equal 1"
 
 
-def test_another_using_fixture(my_fixture: str) -> None:
-    my_user_ids = [1, 2, 3]
-    assert len(my_user_ids) == 3, "Length of my_user_ids must be 3"
-    assert my_fixture.upper() == "HELLO, FIXTURE!", f"{my_fixture.upper()} must be equal 'HELLO, FIXTURE!'"
+def test_tool():
+    my_a: int = 5
+    assert Tool.a == my_a, f"{Tool.a} must be equal {my_a}"
 
 
+class TestCalculator:
+    @pytest.mark.parametrize(
+        argnames=["x", "y", "res", "expectation"],
+        argvalues=[
+            (1, 2, 3, does_not_raise()),
+            (0, 0, 0, does_not_raise()),
+            (-1, 1, 0, does_not_raise()),
+            (2.5, 2.5, 5.0, does_not_raise()),
+            ("10", 5, None, pytest.raises(TypeError)),  # This will fail since "10" is a string
+        ]
+    )
+    def test_calculator_add(self, x: int | float, y: int | float, res: int | float, expectation: Any) -> None:
+        with expectation:
+            my_rest: float | int = Calculator.add(x, y)
+            assert my_rest == res, f"{my_rest} must be equal {res}"
 
-@pytest.fixture
-def user_repo() -> UserRepository:
-    """Fixture that provides a UserRepository with some pre-populated users."""
-    users = [
-        User(id=1, email="alice@example.com"),
-        User(id=2, email="bob@example.com"),
-    ]
-    return UserRepository(users=users)
-
-
-# @pytest.mark.usefixtures("user_repo")
-class TestUserRepository:
-    def test_get_by_email_returns_user(self, user_repo: UserRepository):
-        """Test successful retrieval of a user by email."""
-        user: Optional[User] = user_repo.get_by_email("alice@example.com")
-        assert user is not None
-        assert user.id >= 1
-        assert user.email != ""
-
-    def test_get_by_email_returns_none_for_unknown_email(self, user_repo: UserRepository):
-        user: Optional[User] = user_repo.get_by_email("unknown@example.com")
-        assert user is None
-
-    def test_deactivate_marks_user_as_inactive(self, user_repo: UserRepository):
-        """Test successful deactivation of a user."""
-
-        # Get user before deactivation
-        user: Optional[User] = user_repo.get_by_email("alice@example.com")
-
-        # Make sure user exists before deactivating and checking status is active
-        assert user is not None
-        assert user.is_active is True
-
-        # Deactivate user
-        user_repo.deactivate(user_id=user.id)
-        assert user.is_active is False
-
-
-def test_we_are_in_debug_mode(enable_debug_mode: Settings) -> None:
-    assert enable_debug_mode.debug is True, "Debug mode must be enabled in this test"
+    @pytest.mark.parametrize(
+        argnames=["x", "y", "res", "expectation"],
+        argvalues=[
+            (4, 2, 2.0, does_not_raise()),
+            (5, 2, 2.5, does_not_raise()),
+            (1, 0, None, pytest.raises(ZeroDivisionError)),
+        ]
+    )
+    def test_calculator_divide(self, x: int | float, y: int | float, res: float, expectation: Any) -> None:
+        with expectation:
+            my_res: float = Calculator.divide(x, y)
+            assert my_res == res, f"{my_res} must be equal {res}"
