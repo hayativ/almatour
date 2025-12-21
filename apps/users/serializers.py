9f1a04097e00890cc7f0373ad322f3d0ae0cf376
Ева <davydova.04.eva@mail.ru>
2025-12-22 02:32:32 +0500

    Add commets, type hinting, PEP 8

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,12 +1,17 @@
-from rest_framework import serializers
+from typing import Any
+
 from django.contrib.auth import get_user_model
-from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    """Serializer for user registration."""
+
     email = serializers.EmailField(
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
@@ -21,12 +26,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('email', 'username', 'password',
                   'password2', 'phone', 'is_seller', 'address')
 
-    def validate(self, attrs):
+    def validate(self, attrs: dict[str, Any]) -> dict[str, Any]:
+        """Validate that passwords match."""
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Wrong password."})
         return attrs
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict[str, Any]) -> Any:
+        """Create a new user with validated data."""
         validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
